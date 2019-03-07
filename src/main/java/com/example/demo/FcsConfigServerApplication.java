e1969e61e6cb041fd828368c8807d43d26aaef95
liuguoqing@sxwdsoft.com <liuguoqing@sxwdsoft.com>
2019-03-07 18:02:44 +0800

    增加config配置中心，配置文件统一从git获取

--- src/main/java/com/example/demo/FcsConfigServerApplication.java
+++ src/main/java/com/example/demo/FcsConfigServerApplication.java
@@ -2,12 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-public class FcsConfigserverApplication {
+@EnableConfigServer
+public class FcsConfigServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FcsConfigserverApplication.class, args);
+		SpringApplication.run(FcsConfigServerApplication.class, args);
 	}
 
 }
