b7c25b67f42862b64424564348294a363655b41c
liuguoqing@sxwdsoft.com <liuguoqing@sxwdsoft.com>
2019-03-15 16:15:49 +0800

    增加zuul路由转发功能，zuul本身集成ribbon，单独的ribbon工程可以作废

--- src/test/java/com/example/demo/FcsConfigServerApplicationTests.java
+++ src/test/java/com/example/demo/FcsConfigServerApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class FcsConfigserverApplicationTests {
+public class FcsConfigServerApplicationTests {
 
 	@Test
 	public void contextLoads() {
